8cd1c1b59657c6bf15efd64f1eb6e311d7576784
sandrafong <sandrafong90@hotmail.com>
2022-09-20 11:40:41 +0800

    fix: cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(cors({
-  origin: "https://sandrafongshurui.github.io/"
+  origin: "https://sandrafongshurui.github.io"
 
 }))
 
